06e898ce4a014e0537eafc832d1729599d02caaa
Shikhar Verma <shikharv1000@gmail.com>
2026-04-17 00:22:39 -0700

    vite scaffolding

---
+++ nc-blast/src/App.jsx
@@ -0,0 +1,12 @@
+import { CHANGELOG } from './data/changelog';
+import { GUIDE_SECTIONS } from './data/guide';
+import { DEFAULT_PARTS, TOP10, BLADE_COLORS, CROSSOVER_BLADES } from './data/parts';
+import { QUICK_COMBOS } from './data/quickCombos';
+import { FINISH, PENALTY } from './data/finishes';
+import { KEYS, sGet, sSave, mergeWithDefaults } from './lib/storage';
+import { emptyCombo, comboStr, comboReady, tn, splitPartName } from './lib/combos';
+
+function App() {
+}
+
+export default App
